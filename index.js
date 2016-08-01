c6bbd56fcc1e5ddbcda3773cbd6091b7fc4aac53
Tobias Büschel <tobias.buschel.15@ucl.ac.uk>
2016-08-01 15:39:55 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var fs = require("fs");
+
+
+// TODO: TWO COMMANDS
+
+// (1) merge - looks for all .dotfiles in given folder and merges them into .masterrc
+// (2) unmerge - splits content of .masterrc and creates respective files
+
+// Usage:
+// runs at the same time as NPM install is run
+
+// you can invoke the tool using:
+- dot init
+- dot merge
+- dot unmerge
+
+// gulp task
+
+// grunt task
