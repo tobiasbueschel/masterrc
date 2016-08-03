fa42344e8fb71c3f7761756b17cee31a8df488bb
Luke Jackson <lukejacksonn@gmail.com>
2016-08-03 00:45:11 -0400

    Writes out individual files from masterrc

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-var fs = require("fs");
+const fs = require('fs');
+
+const lines = fs.readFileSync(__dirname + '/.masterrc', 'utf8').split(/\n/);
+const templates = [];
+let template = {
+  name: '',
+  contents: [],
+}
+lines.forEach(line => {
+  if(line.match(/```/) && template.name === '') {
+    template.name = line.replace(/```/,'');
+  } else if (line.match(/```/)) {
+    template.contents = template.contents.join('\n').trim();
+    templates.push(template);
+    template = {
+      name: '',
+      contents: [],
+    }
+  }
+  else {
+    template.contents.push(line);
+  }
+});
+
+console.log(templates);
+
+templates.forEach(x => {
+  fs.writeFileSync(__dirname + '/' + x.name, x.contents);
+})
